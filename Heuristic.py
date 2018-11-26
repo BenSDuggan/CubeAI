2325fc4393bdf0fc77ab9146af1262800602c790
Ben Duggan <dugganbens@gmail.com>
2018-11-26 15:20:51 -0500

    updating readme documentation

---
+++ Heuristic.py
@@ -0,0 +1,11 @@
+'''
+    Team 4 - Ben Duggan & Connor Altic
+    11/26/18
+    Class containing heuristics that can be used
+'''
+
+class Heuristic:
+
+    @staticmethod
+    def simpleHeuristic():
+        pass
