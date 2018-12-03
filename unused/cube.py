167c4ef74c8dd4555b05208407369ec37ef7aa98
Connor Altic <caltic@iu.edu>
2018-12-03 17:44:57 -0500

    cube

--- unused/cube.py
+++ unused/cube.py
@@ -1,12 +1,12 @@
 from Cube import*
-class Cube:
+class cube:
 
     def __init__(self, map):
         temp = []
-        temp.append([map.state[1][0], map.state[4][0], map.state[5][3]])
-        temp.append([map.state[1][1], map.state[2][1], map.state[5][2]])
-        temp.append([map.state[1][2], map.state[2][0], map.state[0][1]])
         temp.append([map.state[1][3], map.state[4][1], map.state[0][0]])
+        temp.append([map.state[1][2], map.state[2][0], map.state[0][1]])
+        temp.append([map.state[1][1], map.state[2][1], map.state[5][2]])
+        temp.append([map.state[1][0], map.state[4][0], map.state[5][3]])
         temp.append([map.state[3][0], map.state[4][2], map.state[0][3]])
         temp.append([map.state[3][1], map.state[2][3], map.state[0][2]])
         temp.append([map.state[3][2], map.state[2][2], map.state[5][1]])
@@ -24,7 +24,7 @@ class Cube:
 
 class Cublet:
     def __init__(self, a):
-        cublets = [{1,4,5}, {1,2,5}, {1,0,2}, {1,0,4}, {0,4,3}, {3,0,2}, {3,2,5}, {3,4,5}]
+        cublets = [{1,4,0}, {1,2,0}, {1,2,5}, {1,5,4}, {0,4,3}, {3,0,2}, {3,2,5}, {3,4,5}]
         if a[0] == 1 or a[0] == 3:
             self.ore = 0
         if a[1] == 1 or a[1] == 3:
---
+++ unused/myHeuristic.py
@@ -0,0 +1,102 @@
+from Cube import*
+from cube import*
+
+class myHeuristic:
+
+    def __init__(self, c):
+        self.state = c
+
+    def scoreCube(self, this):
+        cube = cube(this)
+        score = self.scorePiece(cube, 0)
+        this.makeMove((1,1))
+        this.makeMove((3,3))
+        cube = cube(this)
+        score = score + self.scorePiece(cube, 1)
+        this.makeMove((1,1))
+        this.makeMove((3,3))
+        cube = cube(this)
+        score = score + self.scorePiece(cube, 2)
+        this.makeMove((1,1))
+        this.makeMove((3,3))
+        score = score + self.scorePiece(cube, 3)
+        this.makeMove((1,1))
+        this.makeMove((3,3))
+        this.makeMove((4,2))
+        this.makeMove((2,2))
+        this.makeMove((1,3))
+        this.makeMove((3,1))
+        cube = cube(this)
+        score = score + self.scorePiece(cube, 4)
+        this.makeMove((1,3))
+        this.makeMove((3,1))
+        cube = cube(this)
+        score = score + self.scorePiece(cube, 5)
+        this.makeMove((1,3))
+        this.makeMove((3,1))
+        cube = cube(this)
+        score = score + self.scorePiece(cube, 6)
+        this.makeMove((1,3))
+        this.makeMove((3,1))
+        cube = cube(this)
+        return score + self.scorePiece(cube,7)
+
+    def scorePiece(self, c, p):
+        index = c.findPiece(p)
+        case = c.cube[index]
+        if case.id == 0:
+            if case.orr == 0:
+                return 0
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 2
+        if case.id == 1:
+            if case.orr == 0:
+                return 1
+            if case.orr == 1:
+                return 1
+            if case.orr == 2:
+                return 2
+        if case.id == 2:
+            if case.orr == 0:
+                return 1
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 2
+        if case.id == 3:
+            if case.orr == 0:
+                return 1
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 1
+        if case.id == 4:
+            if case.orr == 0:
+                return 2
+            if case.orr == 1:
+                return 1
+            if case.orr == 2:
+                return 1
+        if case.id == 5:
+            if case.orr == 0:
+                return 2
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 2
+        if case.id == 6:
+            if case.orr == 0:
+                return 2
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 2
+        if case.id == 7:
+            if case.orr == 0:
+                return 1
+            if case.orr == 1:
+                return 2
+            if case.orr == 2:
+                return 2
